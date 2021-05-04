79e7aa552b26a6493fe9dbfb01245e3925e346f3
Trevor <65122083+TPXS@users.noreply.github.com>
2021-05-04 15:45:37 -0400

    double rotation

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -2,21 +2,27 @@
 #![no_main]
 #![no_std]
 
-use volatile::Volatile;
+
 use aux5::{entry, Delay, DelayMs, LedArray, OutputSwitch};
 
 #[entry]
 fn main() -> ! {
     let (mut delay, mut leds) : (Delay, LedArray) = aux5::init();
 
-    let mut half_period = 500_u16;
-    let v_half_period = Volatile::new(&mut half_period);
-
+    let ms = 25_u8;
+    
     loop {
-        leds[0].on().ok();
-        delay.delay_ms(v_half_period.read());
-        
-        leds[0].off().ok();
-        delay.delay_ms(v_half_period.read());
+        for current in 0..8 {
+            let next = (current + 1) % 4;
+
+            leds[next].on().ok();
+            leds[(next + 4) % 8].on().ok();
+            delay.delay_ms(2 * ms);
+
+            leds[current].off().ok();
+            leds[(current + 4) % 8].off().ok();
+            delay.delay_ms(ms);
+        }
     }
+
 }
